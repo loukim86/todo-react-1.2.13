eae30137ad78aa7a8649560c56590ba748dfd095
loukim86 <148028680+loukim86@users.noreply.github.com>
2024-01-20 22:49:50 +0900

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "../Header/Header";
 import TaskList from "../TaskList/TaskList";
 import Footer from "../Footer/Footer";
@@ -6,39 +6,17 @@ import Footer from "../Footer/Footer";
 import './App.css';
 
 const App = () => {
-    const [taskData, setTaskData] = useState([
+    const taskData = [
         { id: 1, status: 'completed', description: 'Completed task', created: 'created 17 seconds ago' },
         { id: 2, status: 'editing', description: 'Editing task', created: 'created 5 minutes ago' },
         { id: 3, status: '', description: 'Active task', created: 'created 5 minutes ago' }
-    ]);
-
-    const editTask = (id) => {
-      setTaskData((prevTaskData) => {
-          const updatedTaskData = prevTaskData.map((item) => {
-              if (item.id === id) {
-                  const taskStatus = item.status === 'completed' ? '' : 'completed';
-                  return { ...item, status: taskStatus };
-              }
-              return item;
-          });
-          return updatedTaskData;
-      });
-  };
-
-  const deleteTask = (id) => {
-    setTaskData((prevTaskData) => {
-        const updatedTaskData = prevTaskData.filter((item) => item.id !== id);
-        return updatedTaskData;
-    });
-};
+    ];
 
 return (
   <section className="todoapp">
       <Header />
       <section className="main">
-          <TaskList taskData={taskData}
-           onChange={editTask}
-           onDelete={deleteTask} />
+          <TaskList taskData={taskData} />
           <Footer />
       </section>
   </section>
